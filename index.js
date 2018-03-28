bd84d2403eb0122afe78cc8f3e9cf4929fa9755a
Phoebe Lau <phoebelau7@gmail.com>
2018-03-28 20:56:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ class Meal{
     return store.deliveries.filter(delivery => delivery.mealId === this.id)
   }
   customers(){
-    this.deliveries().map(delivery => {
+    return this.deliveries().map(delivery => {
       return delivery.customer()
     })
   }
